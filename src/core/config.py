006ccc1f382b4580a8ed294655f4a3ebb7ea2eb5
OscarRodriguez90 <osrodriguez1111@gmail.com>
2026-04-06 15:18:20 +0200

    mm

--- src/core/config.py
+++ src/core/config.py
@@ -43,7 +43,7 @@ MULTIPLICADOR_ATR_TP = 4.0       # Take Profit = entrada ± (ATR × 4.0)
 
 # --- Vigilancia (Watchlist) ---
 WATCHLIST = [
-    'BTC/USD', 'ETH/USD', 'SOL/USD', 'LTC/USD', 'LINK/USD'
+    'BTC/USD:USD', 'ETH/USD:USD', 'SOL/USD:USD', 'LTC/USD:USD', 'ADA/USD:USD'
 ]
 # Nota: Forex en Alpaca requiere permisos específicos, pero el bot ya lo soporta.
 
